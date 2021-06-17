5cf394eaaf2339d343987e0c6779c60655da9546
krystianlab98 <krystianlab98@gmail.com>
2021-06-17 13:28:10 +0200

    Refactored user entity

--- src/models/User.php
+++ src/models/User.php
@@ -1,30 +1,108 @@
 <?php
 
 class User {
+    private $id;
     private $email;
     private $password;
     private $name;
     private $surname;
+    private $phone;
+    private $address;
 
-    public function __construct(
-        string $email,
-        string $password,
-        string $name,
-        string $surname
-    ) {
+    public function __construct($id, $email, $password, $name, $surname, $phone, $adress)
+    {
+        $this->id = $id;
         $this->email = $email;
         $this->password = $password;
         $this->name = $name;
         $this->surname = $surname;
+        $this->phone = $phone;
+        $this->address = $adress;
     }
 
-    public function getEmail(): string
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+
+    public function getEmail()
     {
         return $this->email;
     }
+
+
+    public function setEmail($email): void
+    {
+        $this->email = $email;
+    }
+
 
     public function getPassword()
     {
         return $this->password;
     }
+
+    public function setPassword($password): void
+    {
+        $this->password = $password;
+    }
+
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+
+
+    public function getSurname()
+    {
+        return $this->surname;
+    }
+
+
+    public function setSurname($surname): void
+    {
+        $this->surname = $surname;
+    }
+
+
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+
+    public function setPhone($phone): void
+    {
+        $this->phone = $phone;
+    }
+
+
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+
+    public function setAddress($address): void
+    {
+        $this->address = $address;
+    }
+
+
+
+
 }
