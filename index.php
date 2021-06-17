289a09a0d0c640c44f7351f443f02de28bbb4dc1
krystianlab98 <krystianlab98@gmail.com>
2021-06-17 21:12:57 +0200

    Added logout register createUser urls

--- index.php
+++ index.php
@@ -8,5 +8,8 @@ $path = parse_url( $path, PHP_URL_PATH);
 Router::get('', 'DefaultController');
 Router::get('login', 'DefaultController');
 Router::post('logged', 'LoginController');
+Router::get("logout", 'LoginController');
+Router::get('register', 'LoginController');
+Router::post('createUser', 'LoginController');
 
 Router::run($path);
