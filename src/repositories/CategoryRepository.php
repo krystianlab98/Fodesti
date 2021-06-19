95c1952e3939cb4e95dcf34554365a9680389161
krystianlab98 <krystianlab98@gmail.com>
2021-06-19 14:58:14 +0200

    Added Category repository and controller with function to display all categories and add new one

---
+++ src/repositories/CategoryRepository.php
@@ -0,0 +1,63 @@
+<?php
+require_once 'Repository.php';
+require_once __DIR__.'/../models/Category.php';
+
+class CategoryRepository extends Repository
+{
+    public function getCategory(int $id): ?Category {
+        $statement = $this->database->connect()->prepare('
+            SELECT * FROM public.categories WHERE id = :id
+        ');
+        $statement->bindParam(':id', $id, PDO::PARAM_INT);
+        $statement->execute();
+
+        $category = $statement->fetch(PDO::FETCH_ASSOC);
+
+        if($category == false) {
+            return null;
+        }
+
+        return new Category(
+            $category['id'],
+            $category['title'],
+            $category['imageName']
+        );
+    }
+
+    public function addCategory(Category $category): void
+    {
+
+        $statement = $this->database->connect()->prepare('
+            INSERT INTO categories(title, image_name)
+            VALUES (?, ?)
+        ');
+
+        $statement->execute([
+            $category->getTitle(),
+            $category->getImageName(),
+
+        ]);
+    }
+
+    public function getCategories(): array
+    {
+        $result = [];
+
+        $statement = $this->database->connect()->prepare('
+            SELECT * FROM categories;
+        ');
+        $statement->execute();
+        $categories = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+        foreach ($categories as $category) {
+            $result[] = new Category(
+                $category['title'],
+                $category['image_name']
+            );
+        }
+        return $result;
+    }
+
+
+
+}
